bf9e175b1eb5e56630eb125ca49b1d138bd2af03
ctrl3d <ctrl3d@gmail.com>
2025-08-08 22:58:58 +0900

    Update assembly defines and add conditional integration for Unity Extensions and LitMotion

--- Assets/UnityUIContainer/Runtime/UIContainerData.cs
+++ Assets/UnityUIContainer/Runtime/UIContainerData.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-#if LITMOTION_SUPPORT
+using UnityEngine;
+#if USE_LIT_MOTION
 using LitMotion;
 #endif
-using UnityEngine;
+#if USE_UNITY_EXTENSIONS
 using AnchorPreset = work.ctrl3d.RectTransformExtensions.AnchorPreset;
+#endif
 
 namespace work.ctrl3d
 {
@@ -11,13 +13,19 @@ namespace work.ctrl3d
     public class UIContainerData
     {
         public string name;
+#if USE_UNITY_EXTENSIONS
         public AnchorPreset anchorPreset = AnchorPreset.UseDefault;
+#endif
         public Vector2 position = Vector2.zero;
         public Vector2 sizeDelta = Vector2.zero;
         public Vector2 offset = Vector2.zero;
+#if USE_LIT_MOTION
         public Ease showEase;
+#endif
         public float showDuration;
+#if USE_LIT_MOTION
         public Ease hideEase;
+#endif
         public float hideDuration;
     }
 }
